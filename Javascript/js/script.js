fd5df61aa91ac1fdd5abf0000b74b538065f09fc
almaocuoc <97887732+almaocuoc@users.noreply.github.com>
2022-02-11 13:02:52 +0100

    update

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -4,10 +4,19 @@ console.log("data d\'avui" + d);
 window.alert(d);
 /* Hola, aviam si funciona aquest comentari
  */
-var a, e, c, b;
-a = 1;
-e = 3;
-c = 5;
-b = "dia avui";
-console.log(a);
-windows.alert(d);
+
+var num1 = 30;
+var num2 = 20;
+//assignació dels números. Canvi a declarar i assignar les operacions
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multi = num1 * num2;
+var divi = num1 / num2;
+var modul = num1 % num2;
+console.log("això és el resultat suma =" + suma);
+console.log("això és el resultat resta =" + resta);
+console.log("això és el resultat multiplicació =" + multi);
+console.log("això és el resultat divisió =" + divi);
+console.log("això és el resultat mòdul =" + modul);
+console.log("això és una num1" + num1++)
+console.log("això és un num1 " + num1--)
