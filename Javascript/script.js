14b0b5c48f8d7fd9d649f5d0201ea5834efa239c
almaocuoc <97887732+almaocuoc@users.noreply.github.com>
2022-02-11 11:40:55 +0100

    afegir

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,3 +4,10 @@ console.log("data d'avui + d");
 window.alert(d);
 /* Hola, aviam si funciona aquest comentari
  */
+var a, e, c, b;
+a = 1
+e = 3
+c = 5
+b = dia avui
+console.log(a)
+windows.alter(d)
