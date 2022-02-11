e3d21e44bafc52501a7e153ce583e59ffba1e32a
almaocuoc <97887732+almaocuoc@users.noreply.github.com>
2022-02-11 11:48:10 +0100

    ksdjfl

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -1,13 +1,13 @@
 const d = new Date();
 document.getElementById("demo").innerHTML = d;
-console.log("data d'avui + d");
+console.log("data d\'avui" + d);
 window.alert(d);
 /* Hola, aviam si funciona aquest comentari
  */
 var a, e, c, b;
-a = 1
-e = 3
-c = 5
-b = dia avui
-console.log(a)
-windows.alter(d)
+a = 1;
+e = 3;
+c = 5;
+b = "dia avui";
+console.log(a);
+windows.alert(d);
