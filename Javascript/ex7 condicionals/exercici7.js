7eaae415fbe15b2613b64e2b5e6f36924d131f64
almaocuoc <97887732+almaocuoc@users.noreply.github.com>
2022-02-25 13:00:59 +0100

    update

--- Javascript/ex7 condicionals/exercici7.js
+++ Javascript/ex7 condicionals/exercici7.js
@@ -1,7 +1,8 @@
 /*1. Imagina’t que vols fer el pagament del carrito de la compra.
 Donades 2 variables totalCarrito, cashDisponible, comprova si tens prou cash
 per pagar el total del carret. 
-2 condicions*/
+2 condicions pots posar parseInt
+*/
 let totalCarrito = prompt("Total carrito")
 let cashDisponible = prompt("cashDisponible")
 
@@ -18,31 +19,3 @@ if(cashDisponible => totalCarrito)
 • ANOMYMOUS: El Usuario es visitante, solo puede ver la información
 Controla les accions que poden fer cadascún d’ells amb un if 
 amb if anidado, if else if... un if dentro del else*/
-
-let user;
-user = "anonymus";
-/*
-if (user == "admin") {
-    console.log("Vostè és administrador, pot editar, eliminar i veure la informació")
-} if (user == "editor") {
-    console.log("Vostè és editor, poot editar els registres")
-} else { 
-    console.log("Vostè és autor, pot registrar usuaris")
-}*/
-
-switch (user) {
-    case "admin":
-        console.log("Vostè és administrador, pot editar, eliminar i veure la informació");
-        break;
-
-    case "editor":
-        console.log("Vostè és editor, pot editar els registres");
-        break;
-    case "autor":
-        console.log("Vostè és autor, pot editar usuaris");
-        break;
-    case "anonymous":
-        console.log("Vostè és visitant");
-        break;
-}
-
