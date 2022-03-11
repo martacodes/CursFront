db07d9106a03081a9900d0d61afa1d0ad6d186e4
almaocuoc <97887732+almaocuoc@users.noreply.github.com>
2022-03-11 13:10:15 +0100

    divendres

---
+++ Projecte/js/index.js
@@ -0,0 +1,44 @@
+//1
+
+function buy(id) {
+  for (let i = 0; i < products.length; i++) {
+    const element = products[i];
+    if (element.id === id) {
+      cart.push(element);
+    }
+  }
+}
+//també es pot fer amb un find.
+/* 
+productBuy */
+
+//2
+function cleanCart() {
+  cart.splice(0, cart.length);
+  return cart;
+}
+//més eficient sense res més.
+//carList.length = 0
+
+function subtotal2() {
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].type === "grocery") {
+      subtotal.grocery.value += cart[i].price; //+ subtotal.grocery.value;
+    }
+    if (cart[i].type === "beauty") {
+      subtotal.beauty.value += cart[i].price; // subtotal.beauty.value;
+    }
+    if (cart[i].type === "clothes") {
+      subtotal.clothes.value += cart[i].price; //+ subtotal.clothes.value;
+    }
+  }
+  return subtotal;
+  //console.table(subtotal)
+}
+
+function calculateTotal() {
+  for (let i = 0; i < subtotal.length; i++) {
+    let total = subtotal.grocery.value + total;
+  }
+  return total;
+}
