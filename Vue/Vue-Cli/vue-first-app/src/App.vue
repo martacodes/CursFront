3648e4585309350b69c290979f3bc00bbe32a6bf
martacodes <99327494+martacodes@users.noreply.github.com>
2022-04-21 12:34:23 +0200

    21/4

--- Vue/Vue-Cli/vue-first-app/src/App.vue
+++ Vue/Vue-Cli/vue-first-app/src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <img alt="analog photograhy" src="./assets/img1.jpg" style="width: 600px">
-  <HelloWorld msg="Marta Farré"/>
-  <first-component />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-//import HelloWorld from './components/HelloWorld.vue'
-import firstComponent from './components/firstComp.vue'
-
-export default {
-  name: 'App',
-  components: {
-   //HelloWorld
-    firstComponent
-  }
-}
-</script>
 
 <style>
 #app {
-  font-family: "Gill Sans Extrabold", Helvetica, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
